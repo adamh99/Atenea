6a2715fc1b7abf0fa56a8a5d31e4399e576663a8
gabmantor <99414397+gabmantor@users.noreply.github.com>
2024-05-24 17:47:12 +0200

    Login Update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -20,6 +20,8 @@
 
 </label>
 
+<button type="submit">LOGIN</button>
+
 
 </form>
 
@@ -41,8 +43,39 @@ data () {
         password: ""
 
     };
+},
+
+methods: {
+
+    async handleLogin(){
+
+        const response = await fetch('http://localhost:3306/login', {
+
+            method: 'POST',
+            headers: {
+
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify({
+
+                email: this.email,
+                password: this.password
+            })
+        });
+
+        if(response.ok){
+
+            const data = await response.json();
+            console.log(data);
+        
+        }else{
+
+            console.error('Iniciar session fallido');
+        }
+    }
 }
 
-}
+};
 
 </script>
