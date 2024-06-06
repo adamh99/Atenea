2853cb28a4ad9124df404c159b2ab52298f700fa
gabmantor <99414397+gabmantor@users.noreply.github.com>
2024-06-06 15:21:27 +0200

    Vue router

--- src/components/FormularioMatricula.vue
+++ src/components/FormularioMatricula.vue
@@ -57,7 +57,7 @@
 
 export default{
 
-name: 'Formulario',
+name: 'FormularioMatricula',
 
 data (){
 
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginMatricula from '../components/LoginMatricula.vue';
+import Status from '../components/Status.vue';
+import FormularioMatricula from '../components/FormularioMatricula.vue';
+
+const routes = [
+
+{
+    path: '/',
+    name: 'LoginMatricula',
+    component: LoginMatricula
+
+
+}
+
+
+
+
+
+
+
+
+
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.Base_URL),
+    routes
+});
+
+router.beforeEach((to, from, next) => {
+
+    const loggedIn = localStorage.getItem('user');
+    
+    if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn){
+
+        next('/');
+    
+    }else{
+
+        next();
+    }
+});
+
+export default router;
