ff8b5a2e7e7751c22ba1373e6b479f2f7e7cf6c0
gabmantor <99414397+gabmantor@users.noreply.github.com>
2024-06-07 23:49:14 +0200

    router vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      <LoginMatricula/>
-      <EstatMatricules/>
+          <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import EstatMatricules from './components/Status.vue'
-import LoginMatricula from './components/LoginMatricula.vue';
 
 export default {
   name: 'App',
-
-  components: {
-    EstatMatricules,
-    LoginMatricula
-  },
 
   data: () => ({
     }),
